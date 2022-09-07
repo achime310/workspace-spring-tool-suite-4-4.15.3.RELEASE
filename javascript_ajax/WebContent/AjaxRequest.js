66297130c8887f1ea817d09c4e556c5dc6151ac1
seoyoung <seoyoung310@gmail.com>
2022-09-07 17:53:50 +0900

    ..

---
+++ javascript_ajax/WebContent/AjaxRequest.js
@@ -0,0 +1,52 @@
+
+//function AjaxRequest(method, url,params) {
+function AjaxRequest(method, url,callbackFunction,params) {
+	/*
+	1. XMLHttpRequest객체생성
+	*/
+	this.xhr = new XMLHttpRequest();		
+	/*
+	2. 요청속성설정 
+	*/
+	this.method = method;
+	this.params = params
+	this.url=url;
+	
+	if(this.method=='GET' &&this.params !=null){
+		this.url=this.url+"?"+this.params;
+	}
+	/*
+	3.응답이 도착했을때 실행되는 콜백함수등록(이벤트프로퍼티에함수등록)
+	*/
+
+	//this.xhr.onreadystatechange=function(){
+	/*
+	this.xhr.onload=function(){
+		callbackFunction(this);
+	}
+	*/
+	/*
+	//this.xhr.onload=callbackFunction; 에서 인자받으려고 아래와 같이 기술
+	this.xhr.onreadystatechange=function(){
+		callbackFunction(this);
+	}
+	*/
+	this.xhr.onload=callbackFunction;
+	/*
+	this.xhr.onload=function () {
+		
+		document.querySelector('#newsDiv').innerHTML=this.xhr.responseText;
+	};
+	*/
+	
+	
+	
+	
+	
+	this.xhr.open(this.method,this.url);
+	this.xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+	/*
+	3.요청
+	*/
+	this.xhr.send((this.method=='POST')? this.params:null);		
+}
