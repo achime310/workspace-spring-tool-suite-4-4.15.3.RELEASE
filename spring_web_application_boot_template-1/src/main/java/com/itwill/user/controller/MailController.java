9fd7d64bf2ed4b1d07cc322180072b33f57835c4
seoyoung <seoyoung310@gmail.com>
2022-10-13 18:44:04 +0900

    ..

--- spring_web_application_boot_template-1/src/main/java/com/itwill/user/controller/MailController.java
+++ spring_web_application_boot_template-1/src/main/java/com/itwill/user/controller/MailController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itwill.user.MailService;
-import com.itwill.user.User;
 
 
 @RestController
