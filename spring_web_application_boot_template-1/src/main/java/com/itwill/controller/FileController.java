63a7f4051bb077c577d3f93f33a825f4115e50fd
seoyoung <seoyoung310@gmail.com>
2022-10-13 13:51:22 +0900

    ..

--- spring_web_application_boot_template-1/src/main/java/com/itwill/controller/FileController.java
+++ spring_web_application_boot_template-1/src/main/java/com/itwill/controller/FileController.java
@@ -11,6 +11,7 @@ import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -60,6 +61,12 @@ public class FileController {
 	public String upload2(Locale locale, Model model) {
 
 		return "upload2";
+	}
+	
+	@RequestMapping(value = "/upload_dragndrop", method = RequestMethod.GET)
+	public String upload_dragndrop(Locale locale, Model model) {
+		
+		return "upload_dragndrop";
 	}
 
 	@RequestMapping(value = "requestupload1")
@@ -161,6 +168,95 @@ public class FileController {
 		}
 
 		return "redirect:/";
+	}
+	
+	/*//살패
+	@ResponseBody
+	@RequestMapping(value = { "upload_dragndrop_action" }, method = RequestMethod.POST, produces = "json/plain;charset=UTF-8")
+	public Map upload_dragndrop_action(MultipartHttpServletRequest mtfRequest, 
+			final HttpServletRequest request, 
+			final HttpServletResponse response) {
+		
+		Map resultMap=new HashMap();
+		
+		int code=1;
+		String url="upload1";
+		String msg="실패";
+		
+		int res = 1;
+		System.out.println("제목 > " + request.getParameter("title"));
+		System.out.println("내용 > " + request.getParameter("contents"));
+		if(mtfRequest != null) {
+			List<MultipartFile> fileList = mtfRequest.getFiles("fileList");
+			for(int i=0; i<fileList.size(); i++) {
+				MultipartFile multi = fileList.get(i);
+				if(multi == null) {
+					res = 0;
+					//return 0;
+				}else if(multi.getSize() == 0) {
+					res = 0;
+					//return 0;
+				}else {
+					code=2;
+					msg = "성공";
+					System.out.println("파일명 : " + multi.getOriginalFilename() + " / 파일 사이즈 : " + multi.getSize());
+				}
+			}
+		}
+			
+		resultMap.put("code", code);
+		resultMap.put("url", url);
+		resultMap.put("msg", msg);
+		resultMap.put("data",res);
+		return resultMap;
+	}
+	*/
+	
+	
+	@RequestMapping("/upload_dragndrop_action")
+	@ResponseBody
+	public Map upload_dragndrop_action(MultipartHttpServletRequest mtfRequest,HttpServletRequest request) throws Exception{
+		Map resultMap=new HashMap();
+		
+		int code=1;
+		String url="upload1";
+		String msg="실패";
+		
+		System.out.println("requestupload1_json 호출");
+		System.out.println("제목 > " + request.getParameter("title"));
+		System.out.println("내용 > " + request.getParameter("contents"));
+		
+		String src = mtfRequest.getParameter("src");
+		System.out.println("src value : " + src);
+		MultipartFile mf = mtfRequest.getFile("file");
+		
+		String path = "C:\\2022-05-JAVA-DEVELOPER\\git-repositories\\workspace-spring-tool-suite-4-4.15.3.RELEASE\\spring_web_application_boot_template-1\\src\\main\\webapp\\img\\user_profile\\";
+		
+		String originFileName = mf.getOriginalFilename(); // 원본 파일 명
+		long fileSize = mf.getSize(); // 파일 사이즈
+		
+		System.out.println("originFileName : " + originFileName);
+		System.out.println("fileSize : " + fileSize);
+		
+		String safeFile = path + System.currentTimeMillis() + originFileName;
+		
+		try {
+			mf.transferTo(new File(safeFile));	//파일저장
+			code=2;
+			msg="성공";
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+			msg="IllegalStateException";
+		} catch (IOException e) {
+			e.printStackTrace();
+			msg="IOException";
+		}
+		
+		resultMap.put("code", code);
+		resultMap.put("url", url);
+		resultMap.put("msg", msg);
+		resultMap.put("data",mf);
+		return resultMap;
 	}
 
 }
